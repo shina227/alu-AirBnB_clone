173ed5619639d57b539f0d8c986d8d6ec08767ba
Shina <m.atete@alustudent.com>
2025-05-14 16:24:34 +0200

    Little changes

--- models/base_model.py
+++ models/base_model.py
@@ -8,17 +8,19 @@ import models
 
 
 class BaseModel:
+    """Defines the BaseModel class"""
+
     def __init__(self, *args, **kwargs):
         time_format = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
         self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+        self.updated_at = self.created_at
 
         if kwargs:
             for key, value in kwargs.items():
                 if key == "__class__":
                     continue
-                elif key == "created_at" or key == "updated_at":
+                elif key in ("created_at", "updated_at"):
                     setattr(self, key, datetime.strptime(value, time_format))
                 else:
                     setattr(self, key, value)
@@ -26,51 +28,19 @@ class BaseModel:
         models.storage.new(self)
 
     def save(self):
-        """
-
-        """
+        """Updates updated_at with the current datetime"""
         self.updated_at = datetime.utcnow()
         models.storage.save()
 
     def to_dict(self):
-        """
-
-        """
+        """Returns a dictionary representation of the instance"""
         inst_dict = self.__dict__.copy()
         inst_dict["__class__"] = self.__class__.__name__
         inst_dict["created_at"] = self.created_at.isoformat()
         inst_dict["updated_at"] = self.updated_at.isoformat()
-
         return inst_dict
 
     def __str__(self):
-        """
-
-        """
+        """Returns a string representation of the instance"""
         class_name = self.__class__.__name__
         return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
-
-
-if __name__ == "__main__":
-    my_model = BaseModel()
-    my_model.name = "My_First_Model"
-    my_model.my_number = 89
-    print(my_model.id)
-    print(my_model)
-    print(type(my_model.created_at))
-    print("--")
-    my_model_json = my_model.to_dict()
-    print(my_model_json)
-    print("JSON of my_model:")
-    for key in my_model_json.keys():
-        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]),
-                                       my_model_json[key]))
-
-    print("--")
-    my_new_model = BaseModel(**my_model_json)
-    print(my_new_model.id)
-    print(my_new_model)
-    print(type(my_new_model.created_at))
-
-    print("--")
-    print(my_model is my_new_model)
